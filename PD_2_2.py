4fe18d4d6999f36e0b4a35dfa49d88929bb36ade
Yulian-Kostyuk <122096456+Yulian-Kostyuk@users.noreply.github.com>
2024-08-09 11:17:06 +0300

    Update PD_2_2.py

--- PD_2_2.py
+++ PD_2_2.py
@@ -5,4 +5,5 @@ if first == second and first == third:
     print('Количество одинаковых чисел: 3')
 elif first == second or first == third or second == third:
     print('Количество одинаковых чисел: 2')
-else: print('Количество одинаковых чисел: 0')
+else: 
+    print('Количество одинаковых чисел: 0')
